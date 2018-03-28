ed351a9f1619d7c430c47a5ba27c96a8bdb16da4
DongjinPark <peter9378@naver.com>
2018-03-28 15:42:24 +0900

    1244 cpp, maybe correct & testcase is incorrect

---
+++ 180323swexpert/1244.cpp
@@ -0,0 +1,116 @@
+﻿/**
+*	SW Expert Academy
+*	No. 1244 최대 상금
+*	@author	peter9378
+*	@date		2018.03.28
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// swap함수 구현, 포인터 사용으로 배열에 바로 적용 가능
+void swap(int* a, int* b)
+{
+	int* temp;
+	temp = a;
+	a = b;
+	b = temp;
+	return;
+}
+
+// selection sort 구현
+int sort(int arr[], int size, int count)
+{
+	int max, now_count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		max = i;
+		bool flag = false;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[max] <= arr[j])
+			{
+				max = j;
+				flag = true;
+			}
+		}
+		if (flag)
+		{
+			swap(arr[max], arr[i]);
+			now_count++;
+			if (now_count == count)
+				return 0;
+		}
+	}
+	return count - now_count;
+}
+
+// ctoi 함수 구현
+int ctoi(char target)
+{
+	if (target == '0')
+		return 0;
+	else if (target == '1')
+		return 1;
+	else if (target == '2')
+		return 2;
+	else if (target == '3')
+		return 3;
+	else if (target == '4')
+		return 4;
+	else if (target == '5')
+		return 5;
+	else if (target == '6')
+		return 6;
+	else if (target == '7')
+		return 7;
+	else if (target == '8')
+		return 8;
+	else if (target == '9')
+		return 9;
+	return -1;
+}
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// 테스트 케이스 개수 입력
+	int T;
+	cin >> T;
+
+	// 테스트 케이스 루프
+	for (int test_case = 1; test_case <= T; test_case++)
+	{
+		// 필요한 데이터 입력
+		string str;
+		int swap_count;
+		int arr[6];
+		cin >> str >> swap_count;
+
+		// string을 int배열로 변환
+		for (int i = 0; i < str.length(); i++)
+			arr[i] = ctoi(str.at(i));
+
+		// 소트
+		int remain_count = sort(arr, str.length(), swap_count);
+
+		// 소트했는데 카운트가 남아있으면
+		if (remain_count)
+		{
+			// 홀수일 경우에만 맨 끝 두자리를 바꿈
+			if (remain_count % 2)
+				swap(arr[str.length() - 1], arr[str.length() - 2]);
+		}
+		
+		// 출력
+		cout << "#" << test_case << " ";
+		for (int i = 0; i < str.length(); i++)
+			cout << arr[i];
+		cout << "\n";
+	}
+	return 0;
+}
