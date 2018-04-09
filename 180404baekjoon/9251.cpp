f5cdb6464395ed440f72eec16a1f01bb1e84ba9c
peter9378 <peter9378@naver.com>
2018-04-10 02:24:39 +0900

    9251 cpp

---
+++ 180404baekjoon/9251.cpp
@@ -0,0 +1,46 @@
+﻿/**
+*	baekjoon_stepbystep
+*	No. 9251 LCS
+*	@author	peter9378
+*	@date		2018.04.10
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+// dp 배열
+int dp[1001][1001];
+
+// max 함수 구현
+int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// 두 문자열 입력
+	string a, b;
+	cin >> a >> b;
+
+	// LCS(Longest Common Sequence)적용
+	for (int i = 0; i < a.length(); i++)
+	{
+		for (int j = 0; j < b.length(); j++)
+		{
+			if (a[i] == b[j])
+				dp[i + 1][j + 1] = dp[i][j] + 1;
+			else
+				dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
+		}
+	}
+
+	// 출력
+	cout << dp[a.length()][b.length()];
+
+	return 0;
+}
