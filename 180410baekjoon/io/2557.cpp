bc4ae411a17ba46a0b0b84dc9c1c694f02df14e4
peter9378 <peter9378@naver.com>
2018-04-19 23:38:40 +0900

    2557 cpp

---
+++ 180410baekjoon/io/2557.cpp
@@ -0,0 +1,12 @@
+﻿/**
+*	baekjoon online judge
+*	No. 2557 Hello World
+*	@author	peter9378
+*	@date		2018.04.19
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World!";
+}
