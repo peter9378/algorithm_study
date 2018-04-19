dd17b0962a5be545a4319e1e1bbd94dc2e474437
peter9378 <peter9378@naver.com>
2018-04-19 23:40:50 +0900

    1000 cpp

---
+++ 180410baekjoon/io/1000.cpp
@@ -0,0 +1,19 @@
+﻿/**
+*	baekjoon online judge
+*	No. 1000 A+B
+*	@author	peter9378
+*	@date		2018.04.19
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
+
+	return 0;
+}
