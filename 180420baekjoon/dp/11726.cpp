843ef5da7b25d8d5f67a584e833ed7f8c3b0aa65
peter9378 <peter9378@naver.com>
2018-04-26 16:06:54 +0900

    11726 cpp

---
+++ 180420baekjoon/dp/11726.cpp
@@ -0,0 +1,33 @@
+﻿/**
+*	baekjoon_stepbystep
+*	No. 11726 2xn 타일링
+*	@author	peter9378
+*	@date		2018.04.26
+*/
+#include <iostream>
+using namespace std;
+
+#define MOD 10007
+
+// dp 배열
+int dp[1001];
+
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	dp[0] = dp[1] = 1;
+
+	for (int i = 2; i <= N; i++)
+		dp[i] = (dp[i - 1] + dp[i - 2])%MOD;
+
+	cout << dp[N];
+
+	return 0;
+}
