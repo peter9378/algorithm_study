6b9edd64ab5630a22315b7311d0dc5946c47d8ce
peter9378 <peter9378@naver.com>
2018-04-27 00:53:25 +0900

    11052 cpp

---
+++ 180420baekjoon/dp/11052.cpp
@@ -0,0 +1,42 @@
+﻿/**
+*	baekjoon online judge
+*	No. 11052 붕어빵 판매하기
+*	@author	peter9378
+*	@date		2018.04.27
+*/
+#include <iostream>
+using namespace std;
+
+int P[1001];
+int dp[1001];
+
+int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// 입력
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+		cin >> P[i];
+
+	// dp 연산
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 1; j <= i; j++)
+			dp[i] = max(dp[i], dp[i - j] + P[j]);
+	}
+
+	// 출력
+	cout << dp[N];
+
+	return 0;
+}
