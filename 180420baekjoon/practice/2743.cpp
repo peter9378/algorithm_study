c1dd9b8093731272b0c4a57aa3ba8f68187fb3d0
peter9378 <peter9378@naver.com>
2018-05-01 11:46:05 +0900

    2743 cpp

---
+++ 180420baekjoon/practice/2743.cpp
@@ -0,0 +1,25 @@
+﻿/**
+*	baekjoon online judge
+*	No. 2743	단어 길이 재기
+*	@author	peter9378
+*	@date		2018.05.01
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// 문자열 입력
+	string str;
+	cin >> str;
+
+	// 출력
+	cout << str.length();
+
+	return 0;
+}
