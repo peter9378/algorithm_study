f48dda79b7db6a6dcab3614b6e172b38cd089e22
peter9378 <peter9378@naver.com>
2018-05-07 20:03:00 +0900

    2089 cpp

---
+++ 180420baekjoon/practice/2089.cpp
@@ -0,0 +1,42 @@
+﻿/**
+*	baekjoon online judge
+*	No. 2089	-2진수
+*	@author	peter9378
+*	@date		2018.05.07
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+void func(long long a)
+{
+	if (a == 0)
+		return;
+	if (a < 0)
+	{
+		func(-((a - 1) / 2));
+		cout << -a % 2;
+	}
+	else
+	{
+		func(-(a / 2));
+		cout << a % 2;
+	}
+}
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long N;
+	cin >> N;
+
+	if (N == 0)
+		cout << N;
+	else
+		func(N);
+
+	return 0;
+}
