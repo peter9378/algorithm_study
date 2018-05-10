d4a63bab9780848f75f59d25a1385eff8cb5ca68
peter9378 <peter9378@naver.com>
2018-05-10 19:08:38 +0900

    11653 cpp

---
+++ 180420baekjoon/practice/11653.cpp
@@ -0,0 +1,33 @@
+﻿/**
+*	baekjoon_stepbystep
+*	No. 11653	소인수분해
+*	@author	peter9378
+*	@date		2018.05.10
+*/
+#include <iostream>
+using namespace std;
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int N;
+	cin >> N;
+
+	for (int i = 2; i*i <= N; i++)
+	{
+		if (N%i == 0)
+		{
+			cout << i << "\n";
+			N /= i;
+			i--;
+		}
+	}
+
+	if (N > 1)
+		cout << N;
+
+	return 0;
+}
