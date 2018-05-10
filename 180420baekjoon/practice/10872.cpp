5f90a25a500003ca4fb09d937a496eb2eec979ed
peter9378 <peter9378@naver.com>
2018-05-10 20:13:07 +0900

    10872 cpp

---
+++ 180420baekjoon/practice/10872.cpp
@@ -0,0 +1,26 @@
+﻿/**
+*	baekjoon_stepbystep
+*	No. 10872	팩토리얼
+*	@author	peter9378
+*	@date		2018.05.10
+*/
+#include <iostream>
+using namespace std;
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	long long N;
+	cin >> N;
+
+	long long result = 1;
+	for (int i = 2; i <= N; i++)
+		result *= i;
+
+	cout << result;
+
+	return 0;
+}
