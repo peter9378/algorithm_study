f8b0e16e8a47746f3e65a228fa54a0f74b544ba0
박동진 <peter9378@naver.com>
2019-03-30 00:46:53 +0900

    2902 cpp

---
+++ 190330baekjoon/2902.cpp
@@ -0,0 +1,29 @@
+﻿/**
+*	baekjoon online judge
+*	No. 2902	KMP는 왜 KMP일까?
+*	@author	peter9378
+*	@date		2019.03.30
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	string str;
+	cin >> str;
+
+	cout << str[0];
+	for (int i = 1; i < str.length(); i++)
+	{
+		if (str[i] == '-')
+			cout << str[i + 1];
+	}
+
+	return 0;
+}
