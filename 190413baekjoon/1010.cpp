892d667b76064782fab20bfc2dba27cad27c09a9
박동진 <peter9378@naver.com>
2019-04-13 23:11:26 +0900

    1010 cpp

---
+++ 190413baekjoon/1010.cpp
@@ -0,0 +1,40 @@
+﻿/**
+*	Baekjoon Online Judge
+*	No. 1010	다리 놓기
+*	@author	peter9378
+*	@date		2019.04.13
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[31][31];
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	int T;
+	cin >> T;
+
+	for (int i = 0; i < 31; i++)
+	{
+		dp[i][0] = dp[i][i] = 1;
+		
+		for (int j = 1; j < i; j++)
+			dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+	}
+
+	while (T--)
+	{
+		int N, M;
+		cin >> N >> M;
+
+		cout << dp[M][N] << "\n";
+	}
+
+	return 0;
+}
