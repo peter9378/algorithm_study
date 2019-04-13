3527c73f7781b34b8649c319df4dba5705d13d3d
박동진 <peter9378@naver.com>
2019-04-13 23:20:09 +0900

    2163 cpp

---
+++ 190413baekjoon/2163.cpp
@@ -0,0 +1,33 @@
+﻿/**
+*	Baekjoon Online Judge
+*	No. 2163	초콜릿 자르기
+*	@author	peter9378
+*	@date		2019.04.13
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[301][301];
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 1; i < 301; i++)
+	{
+		for (int j = 1; j <= i; j++)
+			dp[i][j] = dp[j][i] = i*j - 1;
+	}
+
+	cout << dp[N][M];
+
+
+	return 0;
+}
