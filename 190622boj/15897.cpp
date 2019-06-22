5d220329e5e1b0ad2056311bce42cd37eec44598
박동진 <peter9378@naver.com>
2019-06-22 17:27:09 +0900

    15897 cpp

---
+++ 190622boj/15897.cpp
@@ -0,0 +1,27 @@
+﻿/**
+*	BOJ
+*	No.15897	잘못 구현한 에라토스테네스의 체
+*	@author	peter9378
+*	@date		2019.06.22
+*/
+#include <iostream>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	int N;
+	cin >> N;
+
+	long long count = 0;
+	for (int i = N; i != 0; i = (N - 1) / ((N - 1) / i + 1))
+		count += ((N - 1) / i + 1) * (i - ((N - 1) / ((N - 1) / i + 1)));
+	
+	cout << count;
+
+	return 0;
+}
