22ce5b1c9693bf10aefc1129a1f62a8ddac95e2b
박동진 <peter9378@naver.com>
2019-06-25 20:47:47 +0900

    1085 cpp

---
+++ 190625boj/1085.cpp
@@ -0,0 +1,24 @@
+﻿/**
+*	BOJ
+*	No.1085	직사각형에서 탈출
+*	@author	peter9378
+*	@date		2019.06.25	- Remember 6.25 -
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	int x, y, w, h;
+	cin >> x >> y >> w >> h;
+
+	cout << min(min(x, w - x), min(y, h - y));
+
+	return 0;
+}
