821418078cd6033be21c9d388e2ebcc675354485
박동진 <peter9378@naver.com>
2019-06-27 19:15:15 +0900

    #570 B cpp

---
+++ 190627codeforces/570_B.cpp
@@ -0,0 +1,55 @@
+﻿/**
+*	Code Forces
+*	#570 B.	Equalize Prices
+*	@author	peter9378
+*	@date		2019.06.27
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[101];
+
+void init()
+{
+	for (int i = 0; i < 101; i++)
+		arr[i] = 0;
+	return;
+}
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int n, k;
+		cin >> n >> k;
+
+		init();
+		int min_number = 987654321, max_number = -1;
+		double sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+			sum += arr[i];
+			min_number = min(min_number, arr[i]);
+			max_number = max(max_number, arr[i]);
+		}
+
+		if (max_number - 2 * k > min_number)
+			cout << "-1\n";
+		else if ((max_number - min_number) == 2 * k)
+			cout << max_number - k << "\n";
+		else
+			cout << min_number + k << "\n";
+	}
+
+	return 0;
+}
