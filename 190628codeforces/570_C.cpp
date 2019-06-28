37f7807da6aa3b79290ed01aac41ad16deddd614
박동진 <peter9378@naver.com>
2019-06-28 13:59:47 +0900

    #570 C cpp

---
+++ 190628codeforces/570_C.cpp
@@ -0,0 +1,33 @@
+﻿/**
+*	Code Forces
+*	#570 C.	Computer Game
+*	@author	peter9378
+*	@date		2019.06.28
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		long long k, n, a, b;
+		cin >> k >> n >> a >> b;
+
+		if (k <= n*b)
+			cout << "-1\n";
+		else
+			cout << min((k - 1 - n*b) / (a - b), n) << "\n";
+	}
+
+	return 0;
+}
