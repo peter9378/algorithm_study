0a23f85a682a28b66de028f4aea4e21ae80280a1
박동진 <peter9378@naver.com>
2019-06-30 18:19:47 +0900

    10773 cpp

---
+++ 190630boj/10773.cpp
@@ -0,0 +1,43 @@
+﻿/**
+*	baekjoon online judge
+*	No. 10773	제로
+*	@author	peter9378
+*	@date		2019.06.30
+*/
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	int K;
+	cin >> K;
+
+	stack<int> s;
+	for (int i = 0; i < K; i++)
+	{
+		int temp;
+		cin >> temp;
+
+		if (temp)
+			s.push(temp);
+		else
+			s.pop();
+	}
+
+	long long answer = 0;
+
+	while(!s.empty())
+	{
+		answer += s.top();
+		s.pop();
+	}
+	cout << answer;
+	return 0;
+}
