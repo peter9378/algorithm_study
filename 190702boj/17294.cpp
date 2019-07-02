d660d721597159c911eb044c8f4e661eb97413a0
박동진 <peter9378@naver.com>
2019-07-02 21:54:29 +0900

    17294 cpp

---
+++ 190702boj/17294.cpp
@@ -0,0 +1,42 @@
+﻿/**
+*	BOJ
+*	No.17294	귀여운 수~ε٩(๑> ₃ <)۶з
+*	@author	peter9378
+*	@date		2019.07.02
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	long long int k;
+	cin >> k;
+
+	int diff = k%100/10 - k % 10;
+	if (k < 100)
+	{
+		cout << "◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!";
+		return 0;
+	}
+	else
+	{
+		while (k / 10)
+		{
+			if (k % 100 / 10 - k % 10 != diff)
+			{
+				cout << "흥칫뿡!! <(￣ ﹌ ￣)>";
+				return 0;
+			}
+			k /= 10;
+		}
+	}
+	cout << "◝(⑅•ᴗ•⑅)◜..°♡ 뀌요미!!";
+	return 0;
+}
