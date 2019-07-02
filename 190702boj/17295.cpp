34abbd4903dba85ccc2c657946936776770dc984
박동진 <peter9378@naver.com>
2019-07-02 21:41:32 +0900

    17295 cpp

---
+++ 190702boj/17295.cpp
@@ -0,0 +1,26 @@
+/**
+*	BOJ
+*	No.17295	엔드게임 스포일러
+*	@author	peter9378
+*	@date		2019.07.02
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int arr[501][501];
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	string s;
+	cin >> s;
+	cout << "Avengers: Endgame";
+
+	return 0;
+}
