6dfdb80b3151ef7891b2ca8d248006affe6fd850
박동진 <peter9378@naver.com>
2019-07-03 12:56:35 +0900

    1436 cpp

---
+++ 190703boj/1436.cpp
@@ -0,0 +1,38 @@
+﻿/**
+*	BOJ
+*	No.1436	영화감독 숌
+*	@author	peter9378
+*	@date		2019.07.03
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int arr[10001];
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// input data
+	int N;
+	cin >> N;
+
+	int count = 0;
+	int target = 666;
+	while (count != N)
+	{
+		if (to_string(target).find("666") != string::npos)
+		{
+			count++;
+		}
+		target++;
+	}
+
+	cout << target-1;
+
+	return 0;
+}
