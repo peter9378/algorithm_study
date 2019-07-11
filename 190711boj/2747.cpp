df2d36c25306dca803bda592b70d25e31e224448
박동진 <peter9378@naver.com>
2019-07-11 21:54:37 +0900

    2747 cpp

---
+++ 190711boj/2747.cpp
@@ -0,0 +1,38 @@
+﻿/**
+*	BOJ
+*	No.2747	피보나치 수
+*	@author	peter9378
+*	@date		2019.07.11
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	int a = 0, b = 1;
+	N--;
+	if (N == -1)
+	{
+		cout << 0;
+		return 0;
+	}
+	while (N--)
+	{
+		int temp = a;
+		a = b;
+		b = a + temp;
+	}
+
+	cout << b;
+
+	return 0;
+}
