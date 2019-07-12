6c7e6f37212594bf750a6f21c95ca76c99653a96
박동진 <peter9378@naver.com>
2019-07-12 12:33:43 +0900

    2556 cpp

---
+++ 190712boj/2556.cpp
@@ -0,0 +1,28 @@
+﻿/**
+*	BOJ
+*	No.2556	별 찍기 - 14
+*	@author	peter9378
+*	@date		2019.07.12
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			cout << "*";
+		cout << "\n";
+	}
+	
+	return 0;
+}
