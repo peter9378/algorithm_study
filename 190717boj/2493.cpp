f875b8bf372f3508f18526f22e534f6438ce93b4
박동진 <peter9378@naver.com>
2019-07-17 22:58:26 +0900

    2493 cpp

---
+++ 190717boj/2493.cpp
@@ -0,0 +1,51 @@
+﻿/**
+*	BOJ
+*	No.2493	탑
+*	@author	peter9378
+*	@date		2019.07.17
+*/
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+int arr[500005];
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	stack<pair<int, int>> s;
+	for (int i = 0; i < N; i++)
+	{
+		if (s.empty())
+		{
+			cout << "0 ";
+			s.push(make_pair(i + 1, arr[i]));
+		}
+		else
+		{
+			while (s.top().second < arr[i])
+			{
+				s.pop();
+				if (s.empty())
+					break;
+			}
+			if (s.empty())
+				cout << "0 ";
+			else
+				cout << s.top().first << " ";
+			s.push(make_pair(i + 1, arr[i]));
+		}
+	}
+
+	return 0;
+}
