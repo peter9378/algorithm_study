04b690deece9a6b7566df01554db624a226c9669
박동진 <peter9378@naver.com>
2019-07-17 16:32:22 +0900

    10546 cpp

---
+++ 190717boj/10546.cpp
@@ -0,0 +1,33 @@
+﻿/**
+*	BOJ
+*	No.10546	배부른 마라토너
+*	@author	peter9378
+*	@date		2019.07.17
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+char answer[21];
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	string temp;
+	for (int i = 0; i < 2*N-1; i++)
+	{
+		cin >> temp;
+		for (int j = 0; j < temp.length(); j++)
+			answer[j] ^= temp[j];
+	}
+	
+	cout << answer;
+	return 0;
+}
