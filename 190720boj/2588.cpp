9d2862f99b4bd5656bb1f95ba7283810cddaa8b7
박동진 <peter9378@naver.com>
2019-07-20 13:46:25 +0900

    2588 cpp

---
+++ 190720boj/2588.cpp
@@ -0,0 +1,31 @@
+﻿/**
+*	BOJ
+*	No.2588	곱셈
+*	@author	peter9378
+*	@date		2019.07.20
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+
+	int answer = a * b;
+	while (b)
+	{
+		cout << a * (b % 10) << "\n";
+		b /= 10;
+	}
+
+	cout << answer;
+
+	return 0;
+}
