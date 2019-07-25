dcfd9c9a566c5df901a2f8fbe47b4859faec7ff1
박동진 <peter9378@naver.com>
2019-07-25 17:30:11 +0900

    11050 cpp

---
+++ 190725boj/11050.cpp
@@ -0,0 +1,31 @@
+﻿/**
+*	BOJ
+*	No.11050	이항 계수1
+*	@author	peter9378
+*	@date		2019.07.25
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, K;
+	cin >> N >> K;
+
+	int answer = 1;
+	for (int i = 0; i < K; i++)
+		answer *= (N-i);
+	while (K)
+	{
+		answer /= K;
+		K--;
+	}
+	cout << answer;
+
+	return 0;
+}
