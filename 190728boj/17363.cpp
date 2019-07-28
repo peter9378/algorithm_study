67a4a99a650aa360244b84c66b6ce7d7af045525
박동진 <peter9378@naver.com>
2019-07-28 15:23:54 +0900

    17363 cpp

---
+++ 190728boj/17363.cpp
@@ -0,0 +1,64 @@
+﻿/**
+*	BOJ
+*	No.17362	수학은 체육과목 입니다2
+*	@author	peter9378
+*	@date		2019.07.28
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+char arr[101][101];
+char arrr[101][101];
+
+char convert(char c)
+{
+	switch (c)
+	{
+	case '-':
+		return '|';
+	case '|':
+		return '-';
+	case '/':
+		return '\\';
+	case '\\':
+		return '/';
+	case '^':
+		return '<';
+	case '<':
+		return 'v';
+	case 'v':
+		return '>';
+	case '>':
+		return '^';
+	}
+	return c;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			arrr[M-j-1][i] = convert(arr[i][j]);
+	}
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+			cout << arrr[i][j];
+		cout << "\n";
+	}
+
+	return 0;
+}
