9ec8adfe7d20ac8dbbdb38d415a1d1dd8f0ce96f
박동진 <peter9378@naver.com>
2019-07-28 15:25:25 +0900

    17362 cpp

---
+++ 190728boj/17362.cpp
@@ -0,0 +1,24 @@
+﻿/**
+*	BOJ
+*	No.17362	수학은 체육과목 입니다2
+*	@author	peter9378
+*	@date		2019.07.28
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s = "12345432";
+	int n;
+	cin >> n;
+	cout << s[(n - 1) % 8];
+
+	return 0;
+}
