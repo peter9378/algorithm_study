a39e53342c186048f1aff8c9208a734243d413b3
박동진 <peter9378@naver.com>
2019-08-08 19:57:20 +0900

    2164 cpp

---
+++ 190808boj/2164.cpp
@@ -0,0 +1,35 @@
+﻿/**
+*	BOJ
+*	No.2164	카드2
+*	@author	peter9378
+*	@date		2019.0808
+*/
+#include <iostream>
+#include <algorithm>
+#include <deque>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	deque<int> d;
+	for (int i = 0; i < N; i++)
+		d.push_back(i + 1);
+
+	while (d.size() != 1)
+	{
+		d.pop_front();
+		d.push_back(d[0]);
+		d.pop_front();
+	}
+
+	cout << d[0];
+
+	return 0;
+}
