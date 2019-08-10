68e5b8983db1073ae61708e104720217d37ce4f3
박동진 <peter9378@naver.com>
2019-08-10 23:15:37 +0900

    2740 cpp

---
+++ 190810boj/2740.cpp
@@ -0,0 +1,61 @@
+﻿/**
+*	BOJ
+*	No.2740	행렬 곱셈
+*	@author	peter9378
+*	@date		2019.08.10
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int arr[101][101];
+int arrr[101][101];
+int answer[101][101];
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			cin >> arr[i][j];
+	}
+
+	int K;
+	cin >> M >> K;
+
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < K; j++)
+			cin >> arrr[i][j];
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < K; j++)
+		{
+			for (int k = 0; k < M; k++)
+				answer[i][j] += arr[i][k] * arrr[k][j];
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < K; j++)
+			cout << answer[i][j] << " ";
+		cout << "\n";
+	}
+
+
+	
+
+	return 0;
+}
