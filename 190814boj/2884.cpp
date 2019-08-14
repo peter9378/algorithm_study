556646051f73fc3d0b61f1b70a8d1c2597347147
박동진 <peter9378@naver.com>
2019-08-14 16:44:55 +0900

    2884 cpp

---
+++ 190814boj/2884.cpp
@@ -0,0 +1,32 @@
+﻿/**
+*	BOJ
+*	No.2884	알람 시계
+*	@author	peter9378
+*	@date		2019.08.14
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int h, m;
+	cin >> h >> m;
+
+	int total = h * 60 + m;
+	if (total < 45)
+		cout << 23 << " " << m + 15;
+	else
+	{
+		total -= 45;
+		cout << total / 60 << " " << total % 60;
+	}
+
+	return 0;
+}
