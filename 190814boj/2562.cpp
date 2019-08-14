56fe04033f644c1d1b67025b33b3853bc16e3f4a
박동진 <peter9378@naver.com>
2019-08-14 16:47:26 +0900

    2562 cpp

---
+++ 190814boj/2562.cpp
@@ -0,0 +1,39 @@
+﻿/**
+*	BOJ
+*	No.2562	최댓값
+*	@author	peter9378
+*	@date		2019.08.14
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int arr[10];
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	for (int i = 0; i < 9; i++)
+		cin >> arr[i];
+
+	int max = -1;
+	int max_index = -1;
+
+	for (int i = 0; i < 9; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+			max_index = i;
+		}
+	}
+
+	cout << max << endl << max_index+1;
+
+	return 0;
+}
