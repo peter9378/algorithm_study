22bb26fe7685cf25b86fee71bd74d4308d267b59
박동진 <peter9378@naver.com>
2019-08-14 16:53:34 +0900

    15596 cpp

---
+++ 190814boj/15596.cpp
@@ -0,0 +1,15 @@
+﻿/**
+*	BOJ
+*	No.15596	정수 N개의 합
+*	@author	peter9378
+*	@date		2019.08.14
+*/
+#include <vector>
+
+long long sum(std::vector<int> &a)
+{
+	long long result = 0;
+	for (std::vector<int>::iterator iter = a.begin(); iter != a.end(); iter++)
+		result += *iter;
+	return result;
+}
