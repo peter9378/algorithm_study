a87f548e073fdc0e920412a1fe2ad7e10eb59fd7
박동진 <peter9378@naver.com>
2019-08-17 00:23:42 +0900

    1309 cpp

---
+++ 190816boj/1309.cpp
@@ -0,0 +1,30 @@
+﻿/**
+*	BOJ
+*	No.1309	동물원
+*	@author	peter9378
+*	@date		2019.08.16
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+long long arr[100001];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	arr[0] = 1;
+	arr[1] = 3;
+	for (int i = 2; i < 100001; i++)
+		arr[i] = (2 * arr[i - 1] + arr[i - 2])%9901;
+
+	int N;
+	cin >> N;
+	cout << arr[N];
+
+	return 0;
+}
