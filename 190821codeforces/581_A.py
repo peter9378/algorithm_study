2230f9f3c720998aa20c724b5dee3ce92c818062
박동진 <peter9378@naver.com>
2019-08-21 18:57:32 +0900

    #581 A py

---
+++ 190821codeforces/581_A.py
@@ -0,0 +1,21 @@
+﻿#	Codeforces
+#	#581		A. BowWow and the Timetable
+#	@author	peter9378
+#	@date		2019.08.21
+
+s = input()
+number = 0
+index = 0
+
+for i in range(len(s) - 1, -1, -1) :
+	if s[i] == '1' :
+		number += pow(2, index)
+		index += 1
+
+		answer = 0
+		count = 1
+		while count < number:
+count *= 4
+answer += 1
+
+print(answer)
