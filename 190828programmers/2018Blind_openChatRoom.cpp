0605e3a17836f2f04aee9e42ff1e354d33441bbb
박동진 <peter9378@naver.com>
2019-08-28 19:29:35 +0900

    much faster

--- 190828programmers/2018Blind_openChatRoom.cpp
+++ 190828programmers/2018Blind_openChatRoom.cpp
@@ -4,16 +4,49 @@
 *	@author	peter9378
 *	@date		2019.08.28
 */
-#include <string>
 #include <vector>
-#include <set>
+#include <string>
+#include <map>
 
 using namespace std;
 
+
 vector<string> solution(vector<string> record) {
 	vector<string> answer;
-	vector<pair<string, string>> temp;
-	set<string> id_set;
+	map<string, string> m;    // id, name
+
+	for (int i = 0; i < record.size(); i++)
+	{
+		string s = record[i];
+		string command, id, name;
+		// parsing
+		for (int j = 0; j < s.length(); j++)
+		{
+			if (s[j] == ' ')
+			{
+				command = s.substr(0, j);
+				s = s.substr(j + 1);
+				break;
+			}
+		}
+		for (int j = 0; j < s.length(); j++)
+		{
+			if (s[j] == ' ')
+			{
+				id = s.substr(0, j);
+				name = s.substr(j + 1);
+				break;
+			}
+		}
+
+		if (m.find(id) != m.end())
+		{
+			if (command == "Enter" || command == "Change")
+				m[id] = name;
+		}
+		else
+			m[id] = name;
+	}
 
 	for (int i = 0; i < record.size(); i++)
 	{
@@ -42,60 +75,13 @@ vector<string> solution(vector<string> record) {
 		if (command == "Leave")
 		{
 			id = s;
-			for (int j = 0; j < temp.size(); j++)
-			{
-				if (temp[j].second == id)
-				{
-					name = temp[j].first.substr(0, temp[j].first.length() - 1);
-					break;
-				}
-			}
+			answer.push_back(m[id] + "님이 나갔습니다.");
 		}
-
-		// already id exist
-		if (id_set.find(id) != id_set.end())
-		{
-			if (command == "Enter")
-			{
-				// change history
-				for (int i = 0; i < temp.size(); i++)
-				{
-					if (temp[i].second == id)
-						temp[i].first = name + temp[i].first.back();
-				}
-				temp.push_back(make_pair(name + '0', id));
-			}
-			else if (command == "Change")
-			{
-				// change history
-				for (int i = 0; i < temp.size(); i++)
-				{
-					if (temp[i].second == id)
-						temp[i].first = name + temp[i].first.back();
-				}
-			}
-			else if (command == "Leave")
-				temp.push_back(make_pair(name + '1', id));
-		}
-		else    // new id
-		{
-			id_set.insert(id);
-			char cmd = '0';
-			if (command == "Leave")
-				cmd = '1';
-			else if (command == "Change")
-				cmd = '2';
-			temp.push_back(make_pair(name + cmd, id));
-		}
+		else if (command == "Enter")
+			answer.push_back(m[id] + "님이 들어왔습니다.");
 	}
 
-	for (int i = 0; i < temp.size(); i++)
-	{
-		if (temp[i].first.back() == '0')
-			answer.push_back(temp[i].first.substr(0, temp[i].first.length() - 1) + "님이 들어왔습니다.");
-		else if (temp[i].first.back() == '1')
-			answer.push_back(temp[i].first.substr(0, temp[i].first.length() - 1) + "님이 나갔습니다.");
-	}
+
 
 	return answer;
 }
