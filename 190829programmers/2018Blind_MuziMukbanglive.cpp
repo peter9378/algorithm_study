89428698ea33c72345824dde74d67b4f74ddbce7
박동진 <peter9378@naver.com>
2019-08-29 17:06:58 +0900

    2018 kakao BR#1 Muzi's Mukbang Live cpp(Only accuracy)

---
+++ 190829programmers/2018Blind_MuziMukbanglive.cpp
@@ -0,0 +1,32 @@
+﻿/**
+*	Programmers
+*	2018 KAKAO BLIND RECRUIMTMENT#1	무지의 먹방 라이브
+*	@author	peter9378
+*	@date		2019.08.29
+*/
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> food_times, long long k) {
+	long long index = 0;
+	int length = food_times.size();
+	for (long long i = 0; i < k; i++)
+	{
+		food_times[index]--;
+		long long count = 1;
+		while (true)
+		{
+			if (count > length)
+				return -1;
+			if (food_times[(index + count) % length])
+			{
+				index = (index + count) % length;
+				break;
+			}
+			count++;
+		}
+	}
+	return index + 1;
+}
