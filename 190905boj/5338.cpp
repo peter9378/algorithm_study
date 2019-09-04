2806257fa90107a0433b75a2924a3988804b27c9
박동진 <peter9378@naver.com>
2019-09-05 07:00:25 +0900

    5338 cpp

---
+++ 190905boj/5338.cpp
@@ -0,0 +1,29 @@
+﻿/**
+*	BOJ
+*	No.5338	마이크로소프트 로고
+*	@author	peter9378
+*	@date		2019.09.05
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "       _.-;;-._\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-;;-._|\n";
+	cout << "'-..-'|   ||   |\n";
+	cout << "'-..-'|_.-''-._|";
+
+	return 0;
+}
