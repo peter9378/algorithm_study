11c0528155d70918df2f49c33b9b44e74d210a31
박동진 <peter9378@naver.com>
2019-09-11 14:52:20 +0900

    2644 cpp

---
+++ 190911boj/2644.cpp
@@ -0,0 +1,63 @@
+﻿/**
+*	BOJ
+*	No.2644	촌수계산	이항 계수1
+*	@author	peter9378
+*	@date		2019.09.11
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, M, a, b;
+vector<int> chon[101];
+int check[101];
+
+int bfs()
+{
+	queue<int> q;
+	q.push(a);
+
+	while (!q.empty())
+	{
+		int temp = q.front();
+		q.pop();
+
+		if (temp == b)
+			return check[b];
+
+		for (int i = 0; i < chon[temp].size(); i++)
+		{
+			int n = chon[temp][i];
+			if (check[n] == 0)
+			{
+				q.push(n);
+				check[n] = check[temp] + 1;
+			}
+		}
+	}
+	return -1;
+}
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N;
+	cin >> a >> b;
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		int parent, child;
+		cin >> parent >> child;
+		chon[parent].push_back(child);
+		chon[child].push_back(parent);
+	}
+
+	cout << bfs();
+
+	return 0;
+}
