68e9c56e750496cd46ce337e580829c0e7668580
박동진 <peter9378@naver.com>
2019-09-14 23:47:37 +0900

    10214 cpp

---
+++ 190914boj/10214.cpp
@@ -0,0 +1,41 @@
+﻿/**
+*	BOJ
+*	No.10214	Baseball
+*	@author	peter9378
+*	@date		2019.09.14
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int a = 0, b = 0;
+		for (int i = 0; i < 9; i++)
+		{
+			int c, d;
+			cin >> c >> d;
+			a += c;
+			b += d;
+		}
+		if (a > b)
+			cout << "Yonsei\n";
+		else if (a < b)
+			cout << "Korea\n";
+		else
+			cout << "Draw\n";
+	}
+
+	return 0;
+}
