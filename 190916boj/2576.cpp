5650ba6bf1d9213c12f90f1407e648cb7805a92c
박동진 <peter9378@naver.com>
2019-09-16 05:08:37 +0900

    2576 cpp

---
+++ 190916boj/2576.cpp
@@ -0,0 +1,38 @@
+﻿/**
+*	BOJ
+*	No.2576	홀수
+*	@author	peter9378
+*	@date		2019.09.16
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int mini = 101, sum = 0;
+	for (int i = 0; i < 7; i++)
+	{
+		int temp;
+		cin >> temp;
+		if (temp % 2)
+		{
+			if (temp < mini)
+				mini = temp;
+			sum += temp;
+		}
+	}
+
+	if (sum == 0)
+		cout << -1;
+	else
+		cout << sum << "\n" << mini;
+
+	return 0;
+}
