fdb537c9573906d0719dc53b6ca383101689b60d
박동진 <peter9378@naver.com>
2019-09-15 17:25:14 +0900

    10821 cpp

---
+++ 190915boj/10821.cpp
@@ -0,0 +1,25 @@
+﻿/**
+*	BOJ
+*	No.10821	정수의 개수
+*	@author	peter9378
+*	@date		2019.09.15
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s;
+	cin >> s;
+
+	cout << count(s.begin(), s.end(), ',') + 1;
+
+	return 0;
+}
