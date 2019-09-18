becca2e7f5126f7b9ef34c556f2a6cbc1418c44c
박동진 <peter9378@naver.com>
2019-09-18 12:09:50 +0900

    11003 cpp

---
+++ 190918boj/11003.cpp
@@ -0,0 +1,40 @@
+﻿/**
+*	BOJ
+*	No.11003	최솟값 찾기
+*	@author	peter9378
+*	@date		2019.09.18
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+using namespace std;
+
+int arr[5000005];
+
+// main
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, L;
+	cin >> N >> L;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	priority_queue<pair<int, int>> q;
+	for (int i = 0; i < N; i++)
+	{
+		q.push(make_pair(-arr[i], i));
+		while (q.top().second <= i - L)
+			q.pop();
+		cout << -q.top().first << ' ';
+	}
+
+
+	return 0;
+}
