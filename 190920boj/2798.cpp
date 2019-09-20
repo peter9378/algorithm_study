9ae8170be4b6ebdbcee75733f5922aa936e0414f
박동진 <peter9378@naver.com>
2019-09-20 20:11:13 +0900

    2798 cpp

---
+++ 190920boj/2798.cpp
@@ -0,0 +1,54 @@
+﻿/**
+*	BOJ
+*	No.2798	블랙잭
+*	@author	peter9378
+*	@date		2019.09.20
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> v;
+	vector<int> check;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	for (int i = 0; i < N - 3; i++)
+		check.push_back(0);
+	check.push_back(1);
+	check.push_back(1);
+	check.push_back(1);
+	sort(v.begin(), v.end());
+	sort(check.begin(), check.end());
+
+	int answer = -1;
+	do
+	{
+		int sum = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (check[i])
+				sum += v[i];
+		}
+		if (sum <= M)
+			answer = max(answer, sum);
+	} while (next_permutation(check.begin(), check.end()));
+
+	cout << answer;
+
+	return 0;
+}
