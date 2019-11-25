9e1b209d0cef8ce2d7c32c557c9070c1183cbc2c
박동진 <peter9378@naver.com>
2019-11-25 22:29:28 +0900

    18108 cpp

---
+++ 191125boj/18108.cpp
@@ -0,0 +1,30 @@
+/**
+*	BOJ
+*	No.18108	1998년생인 내가 태국에서는 2541년생?!
+*	@author	peter9378
+*	@date		2019.11.25
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int y;
+	cin >> y;
+	cout << y - 543;
+
+	return 0;
+}
