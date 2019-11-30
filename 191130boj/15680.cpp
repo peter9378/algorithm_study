e28eb3fc21cbe38f3acfdb8a7dcd686f190739e4
박동진 <peter9378@naver.com>
2019-11-30 14:58:23 +0900

    15680 cpp

---
+++ 191130boj/15680.cpp
@@ -0,0 +1,35 @@
+/**
+*	BOJ
+*	No.15680	연세대학교
+*	@author	peter9378
+*	@date		2019.11.30
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	if (N)
+		cout << "Leading the Way to the Future";
+	else
+		cout << "YONSEI";
+
+	return 0;
+}
