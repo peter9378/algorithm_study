1f6db4b849721252a5971a6dae2e71c924899072
박동진 <peter9378@naver.com>
2019-11-30 14:59:43 +0900

    16394 cpp

---
+++ 191130boj/16394.cpp
@@ -0,0 +1,31 @@
+/**
+*	BOJ
+*	No.16394	홍익대학교
+*	@author	peter9378
+*	@date		2019.11.30
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	cout << N - 1946;
+
+	return 0;
+}
