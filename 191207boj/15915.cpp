73b428536afadb607871d1301707eaa9e3b1a4b2
박동진 <peter9378@naver.com>
2019-12-07 21:00:08 +0900

    15915 cpp

---
+++ 191207boj/15915.cpp
@@ -0,0 +1,24 @@
+/**
+*	BOJ
+*	No.15915	가위 바위 보 1002
+*	@author	peter9378
+*	@date		2019.12.07
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <bitset>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << 2;
+
+	return 0;
+}
