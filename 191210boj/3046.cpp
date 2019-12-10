bdb5dc0ef928cd75a9fc0b9022d4160cbe2391d5
박동진 <peter9378@naver.com>
2019-12-10 20:33:32 +0900

    3046 cpp

---
+++ 191210boj/3046.cpp
@@ -0,0 +1,32 @@
+/**
+*	BOJ
+*	No.3046	R2
+*	@author	peter9378
+*	@date		2019.12.10
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <cstring>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int R1, S;
+	cin >> R1 >> S;
+
+	cout << 2 * S - R1;
+
+	return 0;
+}
