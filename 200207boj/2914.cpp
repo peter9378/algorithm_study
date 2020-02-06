1a8e7848422c435932a0d4d658a004b4438f387d
박동진 <peter9378@naver.com>
2020-02-07 00:47:01 +0900

    2914 cpp

---
+++ 200207boj/2914.cpp
@@ -0,0 +1,31 @@
+/**
+*	BOJ
+*	No.2914	¿˙¿€±«
+*	@author	peter9378
+*	@date		2020.02.07
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <cstring>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A, I;
+	cin >> A >> I;
+	cout << A * (I - 1) + 1;
+
+	return 0;
+}
