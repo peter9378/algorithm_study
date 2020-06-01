cc20827f25c12e6fcf88f53ba85f5da2c7c9e5de
박동진 <peter9378@naver.com>
2020-06-01 21:55:46 +0900

    16430 cpp

---
+++ 200601boj/16430.cpp
@@ -0,0 +1,33 @@
+/**
+*	BOJ
+*	No.16430	Á¦¸®¿Í Åè
+*	@author	peter9378
+*	@date		2020.06.01
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int A, B;
+	cin >> A >> B;
+	cout << B - A << " " << B;
+
+	return 0;
+}
