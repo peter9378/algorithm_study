72136651bce4a468eb72522df745666f6d169c4e
박동진 <peter9378@naver.com>
2020-07-24 03:25:08 +0900

    1402 cpp

---
+++ 200724boj/1402.cpp
@@ -0,0 +1,40 @@
+/**
+*	BOJ
+*	No.1402	아무래도이문제는A번난이도인것같다
+*	@author	peter9378
+*	@date		2020.07.24
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << "yes\n";
+	}
+
+	return 0;
+}
