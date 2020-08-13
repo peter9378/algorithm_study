6f42d99d0d0f398e375156594d3057ad64a31e2a
박동진 <peter9378@naver.com>
2020-08-13 23:39:46 +0900

    2420 cpp

---
+++ 200813boj/2420.cpp
@@ -0,0 +1,34 @@
+/**
+*	BOJ
+*	No.2420	사파리월드
+*	@author	peter9378
+*	@date		2020.08.13
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+using namespace std;
+
+// main funcntion
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long N, M;
+	cin >> N >> M;
+	cout << abs(N - M);
+
+	return 0;
+}
