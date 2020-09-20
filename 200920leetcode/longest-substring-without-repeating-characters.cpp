20ae4c4326cc8ee1a5130eb730c50954b7b9193f
dongjin-park <dongjin.park@navercorp.com>
2020-09-20 16:31:56 +0900

    longest-substring-without-repeating-characters cpp, use map for better performance, but almost no improvement

--- 200920leetcode/longest-substring-without-repeating-characters.cpp
+++ 200920leetcode/longest-substring-without-repeating-characters.cpp
@@ -3,12 +3,13 @@ public:
     int lengthOfLongestSubstring(string s) {
         int answer = 0;
         for(int i=0;i<s.length();i++) {
-            unordered_set<char> us;
+            unordered_map<char, int> um;
             for(int j=0;i+j<s.length();j++) {
-                if(us.find(s[i+j]) != us.end()) {
+                if(um.find(s[i+j]) != um.end()) {
+                    i = um[s[i+j]];
                     break;
                 }
-                us.insert(s[i+j]);
+                um[s[i+j]] = i+j;
                 answer = max(answer, j + 1);
             }
         }
