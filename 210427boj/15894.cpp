76703b9a9ea5b9de9625bfcdfeecf47d8fcdbe94
박동진 <peter9378@naver.com>
2021-04-27 23:56:18 +0900

    15894 cpp

---
+++ 210427boj/15894.cpp
@@ -0,0 +1,35 @@
+/**
+*	BOJ
+*	No.15894	수학은 체육과목 입니다
+*	@author	peter9378
+*	@date		2021.04.27
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long N;
+	cin >> N;
+	cout << N * 4;
+
+	return 0;
+}
