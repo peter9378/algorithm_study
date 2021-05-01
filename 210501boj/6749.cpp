125daca0cdcfd82506eb0249072b57b3819c3f2d
박동진 <peter9378@naver.com>
2021-05-01 22:43:02 +0900

    6749 cpp

---
+++ 210501boj/6749.cpp
@@ -0,0 +1,35 @@
+/**
+*	BOJ
+*	No.6749	Next in line
+*	@author	peter9378
+*	@date		2021.05.01
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+	cout << 2 * b - a;
+
+	return 0;
+}
