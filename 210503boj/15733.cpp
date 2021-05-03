c2f0ff7cf95ee9515ab08ed752537a39b4ee7411
박동진 <peter9378@naver.com>
2021-05-03 23:54:39 +0900

    15733 cpp

---
+++ 210503boj/15733.cpp
@@ -0,0 +1,33 @@
+/**
+*	BOJ
+*	No.15733	나는 누구인가
+*	@author	peter9378
+*	@date		2021.05.03
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "I'm Sexy";
+
+	return 0;
+}
