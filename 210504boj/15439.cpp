8fef6c5de6b90124f5ba041369931bb7b15a84ea
박동진 <peter9378@naver.com>
2021-05-04 23:57:57 +0900

    15439 cpp

---
+++ 210504boj/15439.cpp
@@ -0,0 +1,35 @@
+/**
+*	BOJ
+*	No.15439	Vera and Outfits
+*	@author	peter9378
+*	@date		2021.05.04
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	cout << N * (N - 1);
+
+	return 0;
+}
