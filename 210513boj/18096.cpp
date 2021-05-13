8599bc4ea6f32c67f939cb987c6932b4846d54e2
박동진 <peter9378@naver.com>
2021-05-13 23:27:01 +0900

    18096 cpp

---
+++ 210513boj/18096.cpp
@@ -0,0 +1,33 @@
+/**
+*	BOJ
+*	No.18096	¬¡¬â¬Ú¬æ¬Þ¬Ö¬ä¬Ú¬é¬Ö¬ã¬Ü¬Ñ¬ñ ¬Þ¬Ñ¬Ô¬Ú¬ñ
+*	@author	peter9378
+*	@date		2021.05.13
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << 1;
+
+	return 0;
+}
