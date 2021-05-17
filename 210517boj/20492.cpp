a15c2a7c447b73dacb665878d783807518c20fe3
박동진 <peter9378@naver.com>
2021-05-17 23:58:29 +0900

    20492 cpp

---
+++ 210517boj/20492.cpp
@@ -0,0 +1,35 @@
+/**
+*	BOJ
+*	No.20492	¼¼±Ý
+*	@author	peter9378
+*	@date		2021.05.17
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long N;
+	cin >> N;
+	cout << N * 78 / 100 << " " << N * 956 / 1000;
+
+	return 0;
+}
