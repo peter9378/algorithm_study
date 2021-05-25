555cfc9b38ee36cf5fb5aa8ba5eaa9753ac260f4
박동진 <peter9378@naver.com>
2021-05-26 00:01:27 +0900

    21300 cpp

---
+++ 210525boj/21300.cpp
@@ -0,0 +1,35 @@
+/**
+*	BOJ
+*	No.21300	Bottle Return
+*	@author	peter9378
+*	@date		2021.05.25
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	cout << 5 * (a + b + c + d + e + f);
+
+	return 0;
+}
