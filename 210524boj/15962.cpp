d143b9fd29a42d39e568d6ffce22ed828aa12e32
박동진 <peter9378@naver.com>
2021-05-25 23:51:20 +0900

    15962 cpp

---
+++ 210524boj/15962.cpp
@@ -0,0 +1,33 @@
+/**
+*	BOJ
+*	No.15962	새로운 시작
+*	@author	peter9378
+*	@date		2021.05.21
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "파이팅!!";
+
+	return 0;
+}
