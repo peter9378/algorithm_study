487220d12b6beb0ab2203c37d7abee5d3b08b356
박동진 <peter9378@naver.com>
2021-05-30 23:21:37 +0900

    16170 cpp

---
+++ 210530boj/16170.cpp
@@ -0,0 +1,33 @@
+/**
+*	BOJ
+*	No.16170	오늘의 날짜는?
+*	@author	peter9378
+*	@date		2021.05.30
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "2021\n05\n30";
+
+	return 0;
+}
