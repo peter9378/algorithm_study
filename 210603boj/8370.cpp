f8c3a08822564efff76d4e3bd0dd43d80176e85e
박동진 <peter9378@naver.com>
2021-06-03 23:35:59 +0900

    8370 cpp

---
+++ 210603boj/8370.cpp
@@ -0,0 +1,35 @@
+/**
+*	BOJ
+*	No.8370	Plane
+*	@author	peter9378
+*	@date		2021.06.03
+*/
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <cstring>
+#include <iterator>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+// main function
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << a * b + c * d;
+
+	return 0;
+}
